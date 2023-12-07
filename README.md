e2d62fe83835f97b5ab8a2a707d1eb6be0929b1c
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-12-07 17:22:40 +0100

    scaffolding

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-comics
+
+creiamo il layout della pagina utilizzando vite e vue3 componendo la pagina in componenti che definiscono la struttura
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderArea from '../src/components/HeaderArea.vue';
+</script>
+
+<template>
+  <HeaderArea />
+</template>
+
+<style lang="scss">
+@use "./partials/variables.scss" as*;
+</style>
