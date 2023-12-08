f65715df938f7f098671a6101e0ea61e441e1f86
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-12-08 14:57:55 +0100

    feat: sezione link terminata

--- src/components/FooterArea.vue
+++ src/components/FooterArea.vue
@@ -2,7 +2,114 @@
 export default {
     data() {
         return {
-
+            dcComics: [
+                {
+                    name: "Characters",
+                    link: "#",
+                },
+                {
+                    name: "Comics",
+                    link: "#",
+                },
+                {
+                    name: "Movies",
+                    link: "#",
+                },
+                {
+                    name: "TV",
+                    link: "#",
+                },
+                {
+                    name: "Games",
+                    link: "#",
+                },
+                {
+                    name: "Videos",
+                    link: "#",
+                },
+                {
+                    name: "News",
+                    link: "#",
+                },
+            ],
+            stores: [
+                {
+                    name: "Shop DC",
+                    link: "#",
+                },
+                {
+                    name: "Shop DC Collectibles",
+                    link: "#",
+                },
+            ],
+            DcInfo: [
+                {
+                    name: "Terms of Use",
+                    link: "#",
+                },
+                {
+                    name: "Privacy policy (New)",
+                    link: "#",
+                },
+                {
+                    name: "Ad Choises",
+                    link: "#",
+                },
+                {
+                    name: "Advertising",
+                    link: "#",
+                },
+                {
+                    name: "Jobs",
+                    link: "#",
+                },
+                {
+                    name: "Subscriptions",
+                    link: "#",
+                },
+                {
+                    name: "Talent Workshops",
+                    link: "#",
+                },
+                {
+                    name: "CPSC Certificates",
+                    link: "#",
+                },
+                {
+                    name: "Ratings",
+                    link: "#",
+                },
+                {
+                    name: "Shop Help",
+                    link: "#",
+                },
+                {
+                    name: "Contact Us",
+                    link: "#",
+                },
+            ],
+            sites: [
+                {
+                    name: "DC",
+                    link: "#",
+                },
+                {
+                    name: "MAD Magazine",
+                    link: "#",
+                },
+                {
+                    name: "DC Kids",
+                    link: "#",
+                },
+                {
+                    name: "DC Universe",
+                    link: "#",
+                },
+                {
+                    name: "DC Power Visa",
+                    link: "#",
+                },
+            ],
         }
     }
 }
@@ -13,13 +120,49 @@ export default {
     <footer>
 
         <!--sezione link utili e informazioni-->
-        <div class="usefull-links flex">
+
+        <div class="container flex">
 
             <!--contenitore link-->
             <div class="box flex">
-                <div class="col"></div>
-                <div class="col"></div>
-                <div class="col"></div>
+                <div class="col">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li v-for="info in dcComics">
+                            <a :href="info.link">
+                                {{ info.name }}
+                            </a>
+                        </li>
+                    </ul>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li v-for="shop in stores">
+                            <a :href="shop.link">
+                                {{ shop.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>DC</h3>
+                    <ul>
+                        <li v-for="info in DcInfo">
+                            <a :href="info.link">
+                                {{ info.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>SITES</h3>
+                    <ul>
+                        <li v-for="site in sites">
+                            <a :href="site.link">
+                                {{ site.name }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
 
             </div>
             <!--/contenitore link-->
@@ -30,7 +173,10 @@ export default {
             <!--/logo DC background-->
 
         </div>
+
         <!--/sezione link utili e informazioni-->
+
+
 
     </footer>
     <!--/background di sfondo-->
@@ -46,34 +192,64 @@ export default {
 
 footer {
     background-image: url("../assets/footer-bg.jpg");
-}
+    object-fit: cover;
 
-//contenitore
-.usefull-links {
-    height: 23.125rem;
-    max-width: 81.25rem;
-    width: 80%;
-    margin: 0 auto;
-    justify-content: space-between;
 
-    //contenitore links
-    .box {
-        padding: 2.5rem 0 3.125rem 0;
+    //contenitore
+    .container {
+        height: 23.125rem;
+        max-width: 81.25rem;
+        width: 80%;
+        margin: 0 auto;
+        justify-content: space-between;
 
-        .col {
-            margin-right: 1.875rem;
+        //contenitore links
+        .box {
+            padding: 2.5rem 0 3.125rem 0;
 
+            .col {
+                margin-right: 1.875rem;
+                flex-direction: column;
+
+                h3 {
+                    color: white;
+                    padding: 15px 0;
+                }
+
+                li {
+
+                    list-style: none;
+                    padding-bottom: .1875rem;
+
+                    a {
+                        text-decoration: none;
+                        color: #887a7c;
+                        font-size: .75rem;
+
+                        &:hover {
+                            color: white;
+                        }
+                    }
+                }
+            }
         }
+
+        // contenitore links 
+
+        //background logo
+        .bg-image {
+            width: 35.2rem;
+            height: 29.375rem;
+            background-image: url("../assets/dc-logo-bg.png");
+            position: relative;
+            top: -100px;
+        }
+
+        //background logo
     }
 
-    // contenitore links 
+    //contenitore
 
-    .bg-image {
-        width: 35.2rem;
-        height: 35.2rem;
-        background-image: url("../assets/dc-logo-bg.png");
-        position: relative;
-        top: -100px;
-    }
+
 }
 </style>
